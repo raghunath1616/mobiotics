fd81919aaf4f3afacf33f3e92317d982b8fac9fa
raghunath1616 <raghunath1616@gmail.com>
2018-10-03 23:04:12 +0530

    new design change for home page

--- src/dumbComponents/Home/index.js
+++ src/dumbComponents/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import styled from "styled-components"
-import { Box } from "@rebass/grid"
+import { Flex, Box } from "@rebass/grid"
 import Container from "container/SearchAgents"
 import Textbox from "dumbComponents/common/UI/Textbox"
 import Button from "dumbComponents/common/UI/Button"
@@ -59,7 +59,7 @@ const StyledTextbox = styled(Textbox)`
 `
 
 const StyledGoogleAutocompleteTextbox = styled(GoogleAutocompleteTextbox)`
-  height: 57px;
+  height: 68px;
   border-left: none;
   border-top: none;
   border-bottom: none;
@@ -67,6 +67,14 @@ const StyledGoogleAutocompleteTextbox = styled(GoogleAutocompleteTextbox)`
   border-bottom-right-radius: 0;
   @media (max-width: 500px) {
     margin-bottom: 10px;
+  }
+  &.placeholder {
+    color: #9a9a9a;
+    font-size: 20px;
+  }
+  &:-moz-placeholder {
+    color: #9a9a9a;
+    font-size: 20px;
   }
 `
 
@@ -81,12 +89,86 @@ const SearchBox = styled.div`
   }
 `
 
+const Wrapper = styled(Flex)`
+  min-height: 90vh;
+  justify-content: center;
+  align-items: center;
+  background: url("https://d2fedz0by71ckz.cloudfront.net/images/hero-unsigned-referral-img.png");
+  background-size: cover;
+  background-position: center center;
+  padding: 15px;
+  flex-direction: column;
+`
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 1290px;
+`
+
+const FormElement = styled(Flex)`
+  background: rgba(0, 0, 0, 0.5);
+  padding: 50px;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const FormInputContainer = styled.div`
+  margin-right: ${props => (props.marginRight ? props.marginRight : "30px")};
+  height: 68px;
+  flex-grow: ${props => props.flexGrow};
+  width: ${props => props.width};
+  min-width: ${props => props.minWidth};
+`
+
+const FormInput = styled.input`
+  height: 100%;
+  background: #fff;
+  border-radius: 0.5px;
+  border: 0;
+  display: block;
+  width: 80%;
+  padding: 0 32px;
+  font-size: 20px;
+  &.placeholder {
+    color: #9a9a9a;
+    font-size: 20px;
+  }
+  &:-moz-placeholder {
+    color: #9a9a9a;
+    font-size: 20px;
+  }
+  &:hover,
+  &:focus {
+    box-shadow: none;
+    outline: 0;
+  }
+`
+
+const SearchButton = styled.button`
+  height: 100%;
+  width: 100%;
+  background: #4272d7;
+  white-space: nowrap;
+  border-radius: 0.5px;
+  font-size: 20px;
+  color: #fff;
+  transition: all 0.2s ease-out, color 0.2s ease-out;
+  border: 0;
+  cursor: pointer;
+  &:hover {
+    background: #2d62d3;
+  }
+`
+
 class Home extends Component {
   state = {
     location: null,
   }
 
-  searchAgents = () => {
+  searchAgents = (event) => {
+    console.log("event", event)
+    event.preventDefault()
     const { history } = this.props
     const { location } = this.state
     history.push("/search-agents", {
@@ -103,11 +185,11 @@ class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        <FlexContainer>
+        {/* <FlexContainer>
           <HeroImage>
-            {/* <Flex alignItems="center" justifyContent="center">
+            <Flex alignItems="center" justifyContent="center">
               <img src="https://s3.amazonaws.com/icons.agentdesks.com/mail_api/areaa_logo.png" alt="AREAA" />
-            </Flex> */}
+            </Flex>
             <Footer>
               <Box width={[1, 1, 3 / 5]}>
                 <SearchBox>
@@ -129,7 +211,29 @@ class Home extends Component {
               </Box>
             </Footer>
           </HeroImage>
-        </FlexContainer>
+        </FlexContainer> */}
+        <Wrapper>
+          <Form onSubmit={this.searchAgents}>
+            <FormElement>
+              <FormInputContainer flexGrow="1">
+                <FormInput id="txtName" type="text" placeholder="Name" />
+              </FormInputContainer>
+              <FormInputContainer width="32.77%" minWidth="180px">
+                <StyledGoogleAutocompleteTextbox
+                  id="txtAutocomplete"
+                  type={["geocode"]}
+                  location={this.handleLocation}
+                  style={{ borderTopLeftRadius: 0, borderBottomleftRadius: 0 }}
+                  placeholder="City, State or Zip"
+                />
+              </FormInputContainer>
+              <FormInputContainer width="180px" marginRight="0">
+                <SearchButton type="submit">Search</SearchButton>
+              </FormInputContainer>
+            </FormElement>
+          </Form>
+          <Heading type="h5">Powered by Radius Agent</Heading>
+        </Wrapper>
       </React.Fragment>
     )
   }
