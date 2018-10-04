9bf02f78cd395039dbfd8068b369781e38e93686
raghunath1616 <raghunath1616@gmail.com>
2018-10-04 12:51:06 +0530

    fixes

--- src/container/SearchAgents/api.js
+++ src/container/SearchAgents/api.js
@@ -2,4 +2,5 @@ import axios from "axios"
 
 export const fetchAgentsAPI = (data = "") => axios.post(`${API_URL}agent/elastic/query`, data)
 
-export const sendAppLinkToDownloadAPI = (data = "") => axios.post(`${API_V2_URL}appDownloadSms`, data)
+export const sendAppLinkToDownloadAPI = (data = "") => axios
+  .post(`${API_V2_URL}appDownloadSms`, data, { "internal-id": "d3abb3c23d1692f2cc9abb85d6f4ce66" })
