02b020f6ffe22122a3aa6d4c72f754d706510a03
raghunath1616 <raghunath1616@gmail.com>
2018-10-13 08:12:00 +0530

    npm package updated

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,6 @@ import UserLogin from "dumbComponents/Forms"
 import withOnboarding from "hoc/withOnboarding"
 import ContactDetails from "dumbComponents/Forms/ContactDetails"
 import UserDetails from "dumbComponents/Forms/UserDetails"
-import { ToastContainer, toast } from "react-toastify"
 
 const Router = () => (
   <Layout>
@@ -14,9 +13,6 @@ const Router = () => (
       <Route path="/contact-details" component={withOnboarding(ContactDetails)} />
       <Route path="/user-details" component={withOnboarding(UserDetails)} />
     </Switch>
-    {/*Main container for the toast. You can configure global
-      config here. Check API here: https://github.com/fkhadra/react-toastify#api */}
-    <ToastContainer position={toast.POSITION.TOP_RIGHT} />
   </Layout>
 )
 
